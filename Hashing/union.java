f2b7f16486d8e92259d78e46e411645133f547cb
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-01-07 17:08:01 +0530

    Update union.java

--- Hashing/union.java
+++ Hashing/union.java
@@ -1,38 +1,56 @@
 package Hashing;
 
+import java.util.HashSet;
+
 public class union {
-    public static void findUnion(int a[], int b[])
-    {
+//    public static void findUnion(int a[], int b[])
+//    {
+//        int m= a.length;
+//        int n= b.length;
+//        int[] c= new int[m+n];
+//        for(int i=0;i<m;i++)
+//        {
+//            c[i]=a[i];
+//        }
+//        for(int i=0;i<n;i++)
+//        {
+//            c[m+i]=b[i];
+//        }
+//        int res=0;
+//        for(int i=0;i<m+n;i++)
+//        {
+//            boolean flag= false;
+//            for(int j=0;j<i;j++)
+//            {
+//                if(c[i]==c[j])
+//                {
+//                    flag=true;
+//                    break;
+//                }
+//            }
+//            if(flag==false)
+//            {
+//                res++;
+//            }
+//        }
+//        System.out.println(res);
+//    }
+// efficient solution
+      public static void findUnion(int a[],int b[])
+      {
         int m= a.length;
         int n= b.length;
-        int[] c= new int[m+n];
+        HashSet<Integer> s= new HashSet<Integer>(); 
         for(int i=0;i<m;i++)
         {
-            c[i]=a[i];
+            s.add(a[i]);
         }
-        for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
         {
-            c[m+i]=b[i];
+            s.add(b[j]);
         }
-        int res=0;
-        for(int i=0;i<m+n;i++)
-        {
-            boolean flag= false;
-            for(int j=0;j<i;j++)
-            {
-                if(c[i]==c[j])
-                {
-                    flag=true;
-                    break;
-                }
-            }
-            if(flag==false)
-            {
-                res++;
-            }
-        }
-        System.out.println(res);
-    }
+        System.out.println(s.size());
+      }
     public static void main(String args[])
     {
         int[] a={15,20,5,15};
