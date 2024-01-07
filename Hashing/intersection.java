218c0cc281ed7e76ebc13db23ae1d70140f9efee
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-01-07 16:37:27 +0530

    Update intersection.java

--- Hashing/intersection.java
+++ Hashing/intersection.java
@@ -1,37 +1,58 @@
 package Hashing;
-
+import java.util.HashSet;
 public class intersection {
-    public static void intersectionDistinct(int a[], int b[])
-    {
+//    public static void intersectionDistinct(int a[], int b[])
+//    {
+//        int m= a.length;
+//        int n= b.length;
+//        int res=0;
+//        for(int i=0;i<m;i++)
+//        {
+//            boolean flag= false;
+//            for(int j=0;j<i;j++)
+//            {
+//                if(a[i]==a[j])
+//                {
+//                    flag= true;
+//                    break;
+//                }
+//            }
+//            if(flag==true)
+//            {
+//                continue;
+//            }
+//            for(int j=0;j<n;j++)
+//            {
+//                if(a[i]==b[j])
+//                {
+//                    res++;
+//                    break;
+//                }
+//            }
+//        }
+//        System.out.println(res);
+//    }
+//efficient solution
+      public static void intersectionDistinct(int a[], int b[])
+      {
         int m= a.length;
         int n= b.length;
-        int res=0;
+        HashSet<Integer> s= new HashSet<Integer>();
         for(int i=0;i<m;i++)
         {
-            boolean flag= false;
-            for(int j=0;j<i;j++)
-            {
-                if(a[i]==a[j])
-                {
-                    flag= true;
-                    break;
-                }
-            }
-            if(flag==true)
-            {
-                continue;
-            }
-            for(int j=0;j<n;j++)
-            {
-                if(a[i]==b[j])
-                {
-                    res++;
-                    break;
-                }
-            }
+            s.add(a[i]);
         }
-        System.out.println(res);
-    }
+       int res=0;
+       for(int j=0;j<n;j++)
+       {
+        if(s.contains(b[j]))
+        {
+            res++;
+            s.remove(b[j]);
+        }
+       }
+       System.out.println(res);
+      }
     public static void main(String args[])
     {
         int[] a={10,15,20,15,30,30,5};
