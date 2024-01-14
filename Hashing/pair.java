d79c6437c01f00fec58fe89a3398bd758c9a111b
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-01-14 15:35:56 +0530

    Update pair.java

--- Hashing/pair.java
+++ Hashing/pair.java
@@ -1,22 +1,38 @@
 package Hashing;
 
+import java.util.HashSet;
+
 public class pair {
-    public static void isPair(int arr[],int sum)
+ //   public static void isPair(int arr[],int sum)
+ //   {
+ //       int n=arr.length;
+ //       for(int i=0;i<n;i++)
+ //       {
+ //           for(int j=i+1;j<n;j++)
+ //           {
+ //               if(arr[i]+arr[j]==sum)
+ //               {
+ //                   System.out.println("yes");
+ //                   return;
+ //               }
+ //           }
+ //       }
+ //       System.out.println("no");
+ //   }
+ //efficient solution
+ public static void isPair(int arr[],int sum)
+ {
+    int n=arr.length;
+    HashSet<Integer>s=new HashSet<Integer>();
+    for(int i=0;i<n;i++)
     {
-        int n=arr.length;
-        for(int i=0;i<n;i++)
+        if(s.contains(sum-arr[i]))
         {
-            for(int j=i+1;j<n;j++)
-            {
-                if(arr[i]+arr[j]==sum)
-                {
-                    System.out.println("yes");
-                    return;
-                }
-            }
+            System.out.println("yes");
         }
-        System.out.println("no");
+        s.add(arr[i]);
     }
+ }
     public static void main(String args[])
     {
         int[] arr={3,2,8,15,-8};
