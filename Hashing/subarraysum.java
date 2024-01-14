b59ae5afc5b7bf0fe82c52c01085b39034c4057b
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-01-14 18:33:16 +0530

    Update subarraysum.java

--- Hashing/subarraysum.java
+++ Hashing/subarraysum.java
@@ -1,23 +1,43 @@
 package Hashing;
 
+import java.util.HashSet;
+
 public class subarraysum {
-    public static void subarraySum(int arr[], int sum)
-    {
-        int n= arr.length;
+ //   public static void subarraySum(int arr[], int sum)
+ //   {
+ //       int n= arr.length;
+ //       for(int i=0;i<n;i++)
+ //       {
+ //           int curr_sum=0;
+ //           for(int j=i;j<n;j++)
+ //           {
+ //               curr_sum +=arr[j];
+ //               if(curr_sum==sum)
+ //               {
+ //                   System.out.println("yes");
+ //                   return;
+ //               }
+ //           }
+ //       }
+ //   }
+ //efficient solution
+     public static void subarraySum(int arr[], int sum)
+     {
+        int n=arr.length;
+        HashSet<Integer> s= new HashSet<Integer>();
+        int pre_sum=0;
         for(int i=0;i<n;i++)
         {
-            int curr_sum=0;
-            for(int j=i;j<n;j++)
-            {
-                curr_sum +=arr[j];
-                if(curr_sum==sum)
-                {
-                    System.out.println("yes");
-                    return;
-                }
-            }
+             pre_sum +=arr[i];
+           if(pre_sum==sum||s.contains(pre_sum-sum))
+           {
+           System.out.println("yes");
+           return;
         }
-    }
+            s.add(pre_sum);
+        }
+        
+     }
     public static void main(String args[])
     {
         int[] arr={5,8,6,13,3,-1};
