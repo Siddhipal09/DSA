215c99c1a10e0531d752b74a16c4c12194652e89
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-01-14 16:20:59 +0530

    Update subarray.java

--- Hashing/subarray.java
+++ Hashing/subarray.java
@@ -1,23 +1,47 @@
 package Hashing;
 
+import java.util.HashSet;
+
 public class subarray {
-    public static void is0Subarray(int arr[])
+//    public static void is0Subarray(int arr[])
+//    {
+//        int n=arr.length;
+//        for(int i=0;i<n;i++)
+//        {
+//            int curr_sum=0;
+//            for(int j=i;j<n;j++)
+//            {
+//                curr_sum +=arr[j];
+//                if(curr_sum==0)
+//                {
+//                    System.out.println("yes");
+//                    return;
+//                }
+//            }
+//        }
+//    }
+//efficient solution
+public static boolean is0Subarray(int arr[])
+{
+    int n=arr.length;
+    HashSet<Integer> s= new HashSet<Integer>();
+    int pre_sum=0;
+    for(int i=0;i<n;i++)
     {
-        int n=arr.length;
-        for(int i=0;i<n;i++)
-        {
-            int curr_sum=0;
-            for(int j=i;j<n;j++)
-            {
-                curr_sum +=arr[j];
-                if(curr_sum==0)
-                {
-                    System.out.println("yes");
-                    return;
-                }
+        pre_sum +=arr[i];
+        if(s.contains(pre_sum)||pre_sum==0)
+        
+          {
+          
+                
+                System.out.println("yes");
+                 return true;
             }
-        }
+        
+        s.add(pre_sum);
     }
+    return false;
+}
     public static void main(String args[])
     {
         int[] arr={4,-3,2,1};
