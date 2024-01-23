360dcea1ee54d0717cef044cbac7c40bc9594214
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-01-23 19:45:41 +0530

    Update palindrome.java

--- Strings/palindrome.java
+++ Strings/palindrome.java
@@ -1,12 +1,28 @@
 package Strings;
 
 public class palindrome {
-    public static void isPal(String str)
+   // public static void isPal(String str)
+   // {
+   //     StringBuilder rev= new StringBuilder(str);
+   //     rev.reverse();
+   //     System.out.println(str.equals(rev.toString()));
+   // }
+   //efficient solution
+   public static void isPal(String str)
+   {
+    int begin=0;
+    int end= str.length()-1;
+    while(begin<end)
     {
-        StringBuilder rev= new StringBuilder(str);
-        rev.reverse();
-        System.out.println(str.equals(rev.toString()));
+        if(str.charAt(begin)!= str.charAt(end))
+        {
+            System.out.println("no");
+        }
+        begin++;
+        end--;
     }
+    System.out.println("yes");
+   }
     public static void main(String args[])
     {
        String str= "ABBA";
