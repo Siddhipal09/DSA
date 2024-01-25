665e67727adbe7a07265f186ded10e393c1f9fb0
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-01-25 16:41:22 +0530

    Update repeating.java

--- Strings/repeating.java
+++ Strings/repeating.java
@@ -1,21 +1,40 @@
 package Strings;
 
 public class repeating {
-    public static void leftmost(String str)
-    {
+//    public static void leftmost(String str)
+//    {
+//        for(int i=0;i<str.length();i++)
+//        {
+//            for(int j=i+1;j<str.length();j++)
+//            {
+//                if(str.charAt(i)==str.charAt(j))
+//                {
+//                    System.out.println(i);
+//                    return;
+//                }
+//            }
+//        }
+//        System.out.println("-1");
+//    }
+//better approach
+       static final int CHAR=256;
+      public static void leftmost(String str)
+      {
+        int[] count= new int[CHAR];
         for(int i=0;i<str.length();i++)
         {
-            for(int j=i+1;j<str.length();j++)
+            count[str.charAt(i)]++;
+        }
+        for(int i=0;i<str.length();i++)
+        {
+            if(count[str.charAt(i)]>1)
             {
-                if(str.charAt(i)==str.charAt(j))
-                {
-                    System.out.println(i);
-                    return;
-                }
+                System.out.println(i);
+                return;
             }
         }
         System.out.println("-1");
-    }
+      }
     public static void main(String args[])
     {
         String str="cabbad";
