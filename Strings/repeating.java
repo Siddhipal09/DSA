10a0ee77d5e2380e5b9fa0bfbc03c8bf4597c9f1
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-01-25 17:34:46 +0530

    Update repeating.java

--- Strings/repeating.java
+++ Strings/repeating.java
@@ -1,5 +1,7 @@
 package Strings;
 
+import java.util.Arrays;
+
 public class repeating {
 //    public static void leftmost(String str)
 //    {
@@ -17,24 +19,47 @@ public class repeating {
 //        System.out.println("-1");
 //    }
 //better approach
-       static final int CHAR=256;
-      public static void leftmost(String str)
-      {
-        int[] count= new int[CHAR];
-        for(int i=0;i<str.length();i++)
-        {
-            count[str.charAt(i)]++;
-        }
-        for(int i=0;i<str.length();i++)
-        {
-            if(count[str.charAt(i)]>1)
+   //  static final int CHAR=256;
+   //   public static void leftmost(String str)
+   //   {
+   //     int[] count= new int[CHAR];
+   //     for(int i=0;i<str.length();i++)
+   //     {
+   //         count[str.charAt(i)]++;
+   //     }
+   //     for(int i=0;i<str.length();i++)
+   //     {
+   //         if(count[str.charAt(i)]>1)
+   //         {
+   //             System.out.println(i);
+   //             return;
+   //         }
+   //     }
+   //     System.out.println("-1");
+   //   }
+   //efficient solution 1
+         static final int CHAR=256;
+         public static void leftmost(String str)
+         {
+           int[] fIndex = new int[CHAR];
+           Arrays.fill(fIndex,-1);
+           int res= Integer.MAX_VALUE;
+           for(int i=0;i<str.length();i++)
+           {
+            int fi=fIndex[str.charAt(i)];
+            if(fi==-1)
             {
-                System.out.println(i);
-                return;
-            }
+                fIndex[str.charAt(i)]=i;  
+            }else
+            res= Math.min(res,fi);
+           }
+           if(res==Integer.MAX_VALUE)
+           {
+            System.out.println("-1");
+           }else{
+           System.out.println(res);
+         }
         }
-        System.out.println("-1");
-      }
     public static void main(String args[])
     {
         String str="cabbad";
