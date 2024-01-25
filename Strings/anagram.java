3e1f1fff552f27b0c51ad9d1c04f34e93292ed2f
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-01-25 15:36:35 +0530

    Create anagram.java

---
+++ Strings/anagram.java
@@ -0,0 +1,26 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class anagram {
+    public static void areAnagram(String S1, String S2)
+    {
+        if(S1.length()!=S2.length())
+        {
+            System.out.println("not anagram");
+        }
+        char a1[]= S1.toCharArray();
+        Arrays.sort(a1);
+        S1= new String(a1);
+        char a2[]= S2.toCharArray();
+        Arrays.sort(a2);
+        S2= new String(a2);
+        System.out.println(S1.equals(S2));
+    }
+    public static void main(String args[])
+    {
+        String S1="listen";
+        String S2="silent";
+        areAnagram(S1, S2);
+    }
+}
