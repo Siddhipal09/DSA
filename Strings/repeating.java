542ef79dbb4cfac0d425e315c4bcde71d7fe29a0
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-01-25 17:47:40 +0530

    Update repeating.java

--- Strings/repeating.java
+++ Strings/repeating.java
@@ -1,6 +1,6 @@
 package Strings;
 
-import java.util.Arrays;
+//import java.util.Arrays;
 
 public class repeating {
 //    public static void leftmost(String str)
@@ -38,28 +38,45 @@ public class repeating {
    //     System.out.println("-1");
    //   }
    //efficient solution 1
-         static final int CHAR=256;
-         public static void leftmost(String str)
-         {
-           int[] fIndex = new int[CHAR];
-           Arrays.fill(fIndex,-1);
-           int res= Integer.MAX_VALUE;
-           for(int i=0;i<str.length();i++)
-           {
-            int fi=fIndex[str.charAt(i)];
-            if(fi==-1)
+     //    static final int CHAR=256;
+     //    public static void leftmost(String str)
+     //    {
+     //      int[] fIndex = new int[CHAR];
+     //      Arrays.fill(fIndex,-1);
+     //      int res= Integer.MAX_VALUE;
+     //      for(int i=0;i<str.length();i++)
+     //      {
+     //       int fi=fIndex[str.charAt(i)];
+     //       if(fi==-1)
+     //       {
+     //           fIndex[str.charAt(i)]=i;  
+     //       }else
+     //       res= Math.min(res,fi);
+     //      }
+     //      if(res==Integer.MAX_VALUE)
+     //      {
+     //       System.out.println("-1");
+     //      }else{
+     //      System.out.println(res);
+     //    }
+     //   }
+     //efficient solution 2
+     static final int CHAR=256;
+     public static void leftmost(String str)
+     {   
+        boolean[] visited= new boolean[CHAR];
+        int res=-1;
+        for(int i=str.length()-1;i>=0;i--)
+        {
+            if(visited[str.charAt(i)])
             {
-                fIndex[str.charAt(i)]=i;  
-            }else
-            res= Math.min(res,fi);
-           }
-           if(res==Integer.MAX_VALUE)
-           {
-            System.out.println("-1");
-           }else{
-           System.out.println(res);
-         }
+                res=i;
+            }else{
+                visited[str.charAt(i)]=true;
+            }
         }
+        System.out.println(res);
+     }
     public static void main(String args[])
     {
         String str="cabbad";
