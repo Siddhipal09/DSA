44f7f3ebdd85264feede99321c3db3da634789e1
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-03-07 19:58:14 +0530

    Update nonrepeating.java

--- Strings/nonrepeating.java
+++ Strings/nonrepeating.java
@@ -1,26 +1,45 @@
 package Strings;
 
 public class nonrepeating {
-          public static void nonRepeating(String str)
+      //    public static void nonRepeating(String str)
+      //    {
+      //      for(int i=0;i<str.length();i++)
+      //      {
+      //          boolean flag= false;
+      //          for(int j=0;j<str.length();j++)
+      //          {
+      //              if(i!=j&& str.charAt(i)==str.charAt(j))
+      //              {
+      //                  flag=true;
+      //                  break;
+      //              }
+      //          }
+      //          if(flag==false)
+      //          {
+      //          System.out.println(i);
+      //          break;
+      //          } 
+      //      }
+      //    }
+      //better solution
+      static final int CHAR=256;
+      public static void nonRepeating(String str)
+      {
+        int[] count= new int[CHAR];
+         for(int i=0;i<str.length();i++)
+         {
+          count[str.charAt(i)]++;
+         }
+         for(int i=0;i<str.length();i++)
+         {
+          if(count[str.charAt(i)]==1)
           {
-            for(int i=0;i<str.length();i++)
-            {
-                boolean flag= false;
-                for(int j=0;j<str.length();j++)
-                {
-                    if(i!=j&& str.charAt(i)==str.charAt(j))
-                    {
-                        flag=true;
-                        break;
-                    }
-                }
-                if(flag==false)
-                {
-                System.out.println(i);
-                break;
-                }
-            }
+           System.out.println(i);
+           return;
           }
+        }
+        System.out.println("-1");
+      }
           public static void main(String args[])
           {
             String str="geeks for geeks";
