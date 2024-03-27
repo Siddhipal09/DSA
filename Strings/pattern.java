60ffe3c592f76d5bbd01bbe5be41b3528ccd6634
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-03-27 18:47:17 +0530

    Update pattern.java

--- Strings/pattern.java
+++ Strings/pattern.java
@@ -1,30 +1,53 @@
 package Strings;
 
 public class pattern {
+ //   public static void patSearching(String txt, String pat)
+ //   {
+ //       int m= pat.length();
+ //       int n= txt.length();
+ //       for(int i=0;i<=n-m;i++)
+ //       {   
+ //           int j;
+ //           for( j=0;j<m;j++)
+ //           {
+ //               if(pat.charAt(j)!=txt.charAt(i+j))
+ //               break;
+ //               
+ //           }
+ //           if(j==m)
+ //           {
+ //               System.out.println(i+" ");
+ //           }
+ //       }
+ //   }
+ //improved naive algorithm for distinct
     public static void patSearching(String txt, String pat)
     {
-        int m= pat.length();
-        int n= txt.length();
-        for(int i=0;i<=n-m;i++)
-        {   
+         int n= txt.length();
+         int m= pat.length();
+         for(int i=0;i<=n-m;)
+         {
             int j;
-            for( j=0;j<m;j++)
+            for(j=0;j<m;j++)
             {
                 if(pat.charAt(j)!=txt.charAt(i+j))
                 break;
-                
             }
             if(j==m)
+            System.out.println(i);
+            if(j==0)
             {
-                System.out.println(i+" ");
+            i++;
+            }else
+            {
+                i=i+j;
             }
-        }
+         }
     }
- 
     public static void main(String args[])
     {
-        String txt="AAAAA";
-        String pat="AAA";
+        String txt="ABCABCD";
+        String pat="ABCD";
         patSearching(txt,pat);
     }
 }
