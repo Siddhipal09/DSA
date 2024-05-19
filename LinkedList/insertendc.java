941b78e09db6369f5c0d3201db048e72e6d38954
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-19 22:23:06 +0530

    Update insertendc.java

--- LinkedList/insertendc.java
+++ LinkedList/insertendc.java
@@ -10,25 +10,43 @@ class Node
     }
 }
 public class insertendc {
-    static Node insertEnd(Node head, int x)
-          {
-            Node temp= new Node(x);
+ //   static Node insertEnd(Node head, int x)
+ //         {
+ //           Node temp= new Node(x);
+ //            if(head==null)
+ //            {
+ //             temp.next= temp;
+ //             return temp;
+ //            }
+ //            else{
+ //               Node curr= head;
+ //               while(curr.next!= head)
+ //               {
+ //                   curr= curr.next;
+ //               }
+ //               curr.next=temp;
+ //               temp.next= head;
+ //               return head;
+ //            }
+// efficient solution
+      static Node insertEnd(Node head, int x)
+       {
+          Node temp= new Node(x);
              if(head==null)
              {
               temp.next= temp;
               return temp;
              }
-             else{
-                Node curr= head;
-                while(curr.next!= head)
-                {
-                    curr= curr.next;
-                }
-                curr.next=temp;
-                temp.next= head;
-                return head;
+             else
+             {
+                temp.next= head.next;
+                head.next=temp;
+                int t= head.data;
+                head.data=temp.data;
+                temp.data=t;
+                return temp;
              }
-}
+            }
 public static void printList(Node head)
     {
         if(head== null)
