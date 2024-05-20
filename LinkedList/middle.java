5e20cb142a7d5510a16185a9be408657e4286a72
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-20 10:46:17 +0530

    Update middle.java

--- LinkedList/middle.java
+++ LinkedList/middle.java
@@ -10,24 +10,40 @@ class Node
     }
 }
 public class middle {
-    static void middleNode(Node head)
-    {
-        if(head== null)
-          return;
-          int count=0;
-          Node curr;
-          for( curr=head;curr!=null;curr=curr.next)
-          {
-            count++;
+  //  static void middleNode(Node head)
+  //  {
+  //      if(head== null)
+  //        return;
+  //        int count=0;
+  //        Node curr;
+  //        for( curr=head;curr!=null;curr=curr.next)
+  //        {
+  //          count++;
 
-          }
-          curr= head;
-          for(int i=0;i<count/2;i++)
-          {
-            curr= curr.next;
-          }
-          System.out.println(curr.data);
+  //        }
+  //        curr= head;
+  //        for(int i=0;i<count/2;i++)
+  //        {
+  //          curr= curr.next;
+  //        }
+  //        System.out.println(curr.data);
+  //  }
+  //efficient solution
+  static void middleNode(Node head)
+  {
+    if(head== null)
+    {
+        return;
     }
+    Node slow= head;
+    Node fast= head;
+    while(fast!= null&&fast.next!= null)
+    {
+        slow= slow.next;
+        fast=fast.next.next;
+    }
+    System.out.println(slow.data);
+  }
     public static void main (String args[])
     {
         Node head= new Node(10);
