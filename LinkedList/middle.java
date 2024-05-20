51defb25cada2886c8d2d849308609664782c72a
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-20 10:32:16 +0530

    Create middle.java

---
+++ LinkedList/middle.java
@@ -0,0 +1,42 @@
+package LinkedList;
+class Node
+{
+    int data;
+    Node next;
+    Node(int x)
+    {
+        data=x;
+        next=null;
+    }
+}
+public class middle {
+    static void middleNode(Node head)
+    {
+        if(head== null)
+          return;
+          int count=0;
+          Node curr;
+          for( curr=head;curr!=null;curr=curr.next)
+          {
+            count++;
+
+          }
+          curr= head;
+          for(int i=0;i<count/2;i++)
+          {
+            curr= curr.next;
+          }
+          System.out.println(curr.data);
+    }
+    public static void main (String args[])
+    {
+        Node head= new Node(10);
+        head.next= new Node(20);
+        head.next.next= new Node(30);
+        head.next.next.next= new Node(40);
+        head.next.next.next.next= new Node(50);
+        head.next.next.next.next.next= new Node(60);
+        middleNode(head);
+    }
+}
+
