cfb3c6d41352e36cc72d782ef8302f11e271e63f
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-20 21:43:33 +0530

    Create reverse.java

---
+++ LinkedList/reverse.java
@@ -0,0 +1,49 @@
+package LinkedList;
+
+import java.util.ArrayList;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int x)
+    {
+        data=x;
+        next=null;
+    }
+}
+public class reverse {
+    static Node reverseList(Node head)
+    {
+        ArrayList <Integer> arr= new ArrayList <Integer>();
+        for(Node curr= head;curr!= null;curr= curr.next)
+        {
+            arr.add(curr.data);
+        }
+        for(Node curr= head;curr!= null;curr= curr.next)
+        {
+            curr.data= arr.remove(arr.size()-1);
+        }
+        return head;
+    }
+    public static void printList(Node head)
+    {
+        Node curr= head;
+        while(curr!= null)
+        {
+            System.out.print(curr.data+ " ");
+            curr=curr.next;
+        }
+    }
+    public static void main (String args[])
+    {
+        Node head= new Node(10);
+        head.next= new Node(20);
+        head.next.next= new Node(30);
+        head.next.next.next= new Node(40);
+        head.next.next.next.next= new Node(50);
+        
+        head= reverseList(head);
+        printList(head);
+}
+}
