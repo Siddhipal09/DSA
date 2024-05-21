42537cd7f66c6e98df49dd9d0d3dda3b95651b4b
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-21 12:39:15 +0530

    Update reversek.java

--- LinkedList/reversek.java
+++ LinkedList/reversek.java
@@ -10,26 +10,58 @@ class Node
     }
 }
 public class reversek {
-     static Node reverseK(Node head ,int k)
+   //recursive
+   //  static Node reverseK(Node head ,int k)
+   //  {
+   //     Node curr= head;
+   //     Node prev= null;
+   //     Node next= null;
+   //     int count=0;
+   //     while(curr!= null && count<k)
+   //     {
+   //         next= curr.next;
+   //         curr.next= prev;
+   //         prev= curr;
+   //         curr= next;
+   //         count++;
+   //     }
+   //     if(next!= null)
+   //     {
+   //         Node rest_head= reverseK(next,k);
+   //         head.next= rest_head;
+   //     }
+   //     return prev;
+   //  }
+   //iterative 
+     static Node reverseK(Node head, int k)
      {
         Node curr= head;
-        Node prev= null;
-        Node next= null;
-        int count=0;
-        while(curr!= null && count<k)
+        Node prevFirst= null;
+        boolean isFirstPass= true;
+        while(curr!= null)
         {
-            next= curr.next;
-            curr.next= prev;
-            prev= curr;
-            curr= next;
-            count++;
+            Node first= curr;
+            Node prev= null;
+            int count=0;
+            while(curr!=null&& count<k)
+            {
+                Node next= curr.next;
+                curr.next= prev;
+                prev= curr;
+                curr= next;
+                count++;
+            }
+            if(isFirstPass)
+            {
+                head= prev;
+                isFirstPass= false;
+            }else{
+                prevFirst.next= prev;
+            }
+                prevFirst= first;
         }
-        if(next!= null)
-        {
-            Node rest_head= reverseK(next,k);
-            head.next= rest_head;
-        }
-        return prev;
+            return head;
+        
      }
      public static void printList(Node head)
     {
