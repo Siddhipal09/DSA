f6fb79f2e6b909d780fa5996b6cfd02cdf818d18
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-26 00:55:26 +0530

    Create nextgreaterelement.java

---
+++ Stack/nextgreaterelement.java
@@ -0,0 +1,28 @@
+package Stack;
+
+public class nextgreaterelement {
+    static void nextGreater(int arr[])
+    {
+        int n= arr.length;
+        for(int i=0;i<n;i++)
+        {
+            int j;
+            for(j= i+1;j<n;j++)
+            {
+                if(arr[j]> arr[i])
+                {
+                    System.out.print(arr[j]+" ");
+                    break;
+                }
+            }
+            if(j==n)
+                System.out.print("-1"+" ");
+            }
+        }
+        public static void main(String args[])
+        {
+            int arr[]={5,15,10,8,6,12,9,18};
+            nextGreater(arr);
+        }
+    }
+
