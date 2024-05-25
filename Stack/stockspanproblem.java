7aa99db4ed6233918bcd458be771db6711403fb8
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-25 22:15:42 +0530

    Create stockspanproblem.java

---
+++ Stack/stockspanproblem.java
@@ -0,0 +1,22 @@
+package Stack;
+
+public class stockspanproblem {
+   static void printspan(int arr[])
+    {
+        int n= arr.length;
+        for(int i=0;i<n;i++)
+        {
+            int span= 1;
+            for(int j= i-1;j>=0 && arr[j]<=arr[i];j--)
+            {
+                span++;
+            }
+            System.out.print(span+" ");
+        }
+    }
+    public static void main(String args[])
+    {
+       int arr[] = {18, 12, 13, 14, 11, 16};
+       printspan(arr);
+    }
+}
