0543b65c29b2bb1cf0fa439f508cd9fc4cce9666
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-26 00:35:55 +0530

    Update previousgreater.java

--- Stack/previousgreater.java
+++ Stack/previousgreater.java
@@ -1,25 +1,44 @@
 package Stack;
 
+import java.util.Stack;
+
 public class previousgreater {
-    static void printPreviousGreater(int arr[])
-    {
+ //  static void printPreviousGreater(int arr[])
+ //   {
+ //       int n= arr.length;
+ //       for(int i=0;i<n;i++)
+ //       {
+ //           int j;
+ //           for(j=i-1;j>=0;j--)
+ //           {
+ //               if(arr[j]> arr[i])
+ //               {
+ //                   System.out.print(arr[j]+" ");
+ //                   break;
+ //               }
+ //           }
+ //           if(j==-1)
+ //           {
+ //                System.out.print("-1"+" ");
+ //           }
+ //       }
+ //   }
+ //efficient solution
+     static void printPreviousGreater(int arr[])
+     {
         int n= arr.length;
-        for(int i=0;i<n;i++)
+        Stack<Integer> s= new Stack<Integer>();
+        s.push(arr[0]);
+        System.out.print("-1"+" ");
+        for(int i=1;i<n;i++)
         {
-            int j;
-            for(j=i-1;j>=0;j--)
-            {
-                if(arr[j]> arr[i])
-                {
-                    System.out.print(arr[j]+" ");
-                    break;
-                }
-            }
-            if(j==-1)
-            {
-                System.out.print("-1"+" ");
-            }
-        }
+            while(s.isEmpty()== false&& s.peek()<= arr[i])
+            s.pop();
+        
+        int pg= s.isEmpty()?-1:s.peek();
+        System.out.print(pg+" ");
+        s.push(arr[i]);
+     }
     }
     public static void main(String args[])
     {
