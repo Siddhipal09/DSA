5fcee4889b16e615acd9b5d77958d36a301ad9cb
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-27 23:47:58 +0530

    Update largesthistogram.java

--- Stack/largesthistogram.java
+++ Stack/largesthistogram.java
@@ -1,34 +1,61 @@
 package Stack;
 
+import java.util.Stack;
+
 public class largesthistogram {
     
 
-static void getMaxArea(int arr[])
+//static void getMaxArea(int arr[])
+//      {
+//        int res=0;
+//        int n= arr.length;
+//        for(int i=0;i<n;i++)
+//        {
+//           int curr= arr[i];
+//           for(int j= i-1;j>=0;j--)
+//           {
+//            if(arr[j]>=arr[i])
+//            {
+//                curr= curr+arr[i];
+//            }
+//            else
+//            break;
+//           }
+//           for(int j=i+1;j<n;j++)
+//           {
+//            if(arr[j]>=arr[i])
+//            {
+//                curr= curr+arr[i];
+//            }
+//            else
+//            break;
+//           }
+//           res= Math.max(res, curr);
+//        }
+//        System.out.println(res);
+//      }
+//
+//efficient solution
+      static void getMaxArea(int arr[])
       {
-        int res=0;
         int n= arr.length;
+        Stack<Integer> s= new Stack<Integer>();
+        int res=0;
         for(int i=0;i<n;i++)
         {
-           int curr= arr[i];
-           for(int j= i-1;j>=0;j--)
-           {
-            if(arr[j]>=arr[i])
+            while(s.isEmpty()== false&& arr[s.peek()]>=arr[i])
             {
-                curr= curr+arr[i];
+               int tp= s.pop();
+               int curr= arr[tp]*(s.isEmpty()?i:(i-s.peek()-1));
+                res= Math.max(res, curr);
             }
-            else
-            break;
-           }
-           for(int j=i+1;j<n;j++)
-           {
-            if(arr[j]>=arr[i])
-            {
-                curr= curr+arr[i];
-            }
-            else
-            break;
-           }
-           res= Math.max(res, curr);
+            s.push(i);
+        }
+        while(s.isEmpty()==false)
+        {
+            int tp= s.pop();
+           int curr=arr[tp]*(s.isEmpty()?n:(n-s.peek()-1));
+            res= Math.max(res, curr);
         }
         System.out.println(res);
       }
