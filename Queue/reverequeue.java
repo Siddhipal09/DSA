f6b3be75aef4633940774bfcb701d1daea12d4d3
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-05-30 14:24:14 +0530

    reverse queue

---
+++ Queue/reverequeue.java
@@ -0,0 +1,33 @@
+package Queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class reverequeue {
+    static void reverseQueue(Queue<Integer>q)
+    {
+        Stack<Integer> s = new Stack<Integer>();
+        while(q.isEmpty()== false)
+        {
+            s.push(q.peek());
+            q.poll();
+        }
+        while(s.isEmpty()==false)
+        {
+            q.offer(s.peek());
+            s.pop();
+        }
+    }
+    public static void main(String args[])
+    {
+        Queue<Integer> q= new LinkedList<Integer>();
+        q.add(10);
+        q.add(5);
+        q.add(15);
+        q.add(20);
+        reverseQueue(q);
+        System.out.print(q);
+
+    }
+}
