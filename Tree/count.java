02f353688d568b7bd734f9ebd42dfe16c226f811
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-06-08 15:43:12 +0530

    Update count.java

--- Tree/count.java
+++ Tree/count.java
@@ -1,13 +1,34 @@
 package Tree;
 
 public class count {
-    static int countNode(Node root)
+ //   static int countNode(Node root)
+ //   {
+ //       if(root== null)
+ //       return 0;
+ //       else{
+ //           return(1+ countNode(root.left)+countNode(root.right));
+ //       }
+ //   }
+ // efficient solution
+    static double countNode(Node root)
     {
-        if(root== null)
-        return 0;
-        else{
-            return(1+ countNode(root.left)+countNode(root.right));
+        int lh=0;
+        int rh= 0;
+        Node curr= root;
+        while(curr!= null)
+        {
+            lh++;
+            curr= curr.left;
         }
+         curr= root;
+        while(curr!= null)
+        {
+            rh++;
+            curr= curr.right;
+        }
+        if(lh== rh)
+        return (Math.pow(2, lh)-1);
+        return (1+ countNode(root.left)+countNode(root.right));
     }
     public static void main(String[] args) {
         Node root = new Node(10);
@@ -15,7 +36,7 @@ public class count {
         root.right = new Node(30);
         root.left.left = new Node(40);
         root.left.right = new Node(50);
-        int count= countNode(root);
+        double count= countNode(root);
         System.out.println(count);
 }
 }
