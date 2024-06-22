a04852381cecc65649588c25ad307895d93d1b0f
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-06-22 15:52:26 +0530

    Update ceilingonleft.java

--- BST/ceilingonleft.java
+++ BST/ceilingonleft.java
@@ -1,26 +1,46 @@
 package BST;
 
+import java.util.TreeSet;
+
 public class ceilingonleft {
-    static void printCeiling(int arr[])
+ //   static void printCeiling(int arr[])
+ //   {
+ //       int n= arr.length;
+ //       System.out.print("-1"+" ");
+ //       for(int i= 1;i<n;i++)
+ //       {
+ //           int diff= Integer.MAX_VALUE;
+ //           for(int j=0;j<i;j++)
+ //           {
+ //               if(arr[j]>=arr[i])
+ //               {
+ //                   diff= Math.min(diff, (arr[j]-arr[i]));
+ //               }
+ //           }
+ //           if(diff== Integer.MAX_VALUE)
+//            System.out.print("-1"+" ");
+//            else
+//            System.out.print((arr[i]+diff)+" ");
+//        }
+//    }
+//efficient solution
+   static void printCeiling(int arr[])
+   {
+    int n= arr.length;
+    System.out.print("-1"+" ");
+    TreeSet <Integer> s= new TreeSet<Integer>();
+    s.add(arr[0]);
+    for(int i=1;i<n;i++)
     {
-        int n= arr.length;
-        System.out.print("-1"+" ");
-        for(int i= 1;i<n;i++)
-        {
-            int diff= Integer.MAX_VALUE;
-            for(int j=0;j<i;j++)
-            {
-                if(arr[j]>=arr[i])
-                {
-                    diff= Math.min(diff, (arr[j]-arr[i]));
-                }
-            }
-            if(diff== Integer.MAX_VALUE)
+        if(s.ceiling(arr[i])!= null)
+        System.out.print(s.ceiling(arr[i])+" ");
+        else{
             System.out.print("-1"+" ");
-            else
-            System.out.print((arr[i]+diff)+" ");
         }
+        s.add(arr[i]);
     }
+
+   }
     public static void main(String[] args) {
         int[] arr={2,8,30,15,25,13};
         printCeiling(arr);
