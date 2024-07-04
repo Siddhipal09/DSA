5267d0d078847dd2a0d67ff9324f3794b3358b10
Siddhipal09 <137754018+Siddhipal09@users.noreply.github.com>
2024-07-05 00:09:43 +0530

    DP Solution

--- DP/minjumps.java
+++ DP/minjumps.java
@@ -1,24 +1,45 @@
 package DP;
 
 public class minjumps {
-    //recursive
-  static int minJumps(int arr[], int n)
+//  DP solution
+   static int minJumps(int arr[], int n)
+   {
+    int[] dp= new int[n];
+    dp[0]=0;
+    for(int i=1;i<n;i++)
+    dp[i]= Integer.MAX_VALUE;
+    for(int i=1;i<n;i++)
     {
-        if(n==1)
-        return 0;
-        int res= Integer.MAX_VALUE;
-        for(int i=0;i<=n-2;i++)
+        for(int j=0;j<i;j++)
         {
-           
-            if(i+arr[i]>=n-1)
+            if(arr[j]+j >=i)
             {
-                int sub_res= minJumps(arr, i+1);
-                if(sub_res!= Integer.MAX_VALUE)
-                res= Math.min(res, sub_res+1);
+                if(dp[j]!= Integer.MAX_VALUE)
+                dp[i] = Math.min(dp[i], dp[j]+1);
             }
         }
-        return res;
     }
+    return dp[n-1];
+    
+   }    
+//  recursive solution
+//  static int minJumps(int arr[], int n)
+//    {
+//        if(n==1)
+//        return 0;
+//        int res= Integer.MAX_VALUE;
+//        for(int i=0;i<=n-2;i++)
+//        {
+//           
+//            if(i+arr[i]>=n-1)
+//            {
+//                int sub_res= minJumps(arr, i+1);
+//                if(sub_res!= Integer.MAX_VALUE)
+//                res= Math.min(res, sub_res+1);
+//            }
+//        }
+//        return res;
+//    }
     public static void main(String[] args) {
         int[] arr={3,4,2,1,2,1};
         int n= arr.length;
